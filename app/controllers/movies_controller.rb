513121daba49198a068cefddb1a754690fa35977
Jomiro <jomiro.eming@gmail.com>
2019-09-21 20:47:37 +0200

    Added some notes to clarify my thinking, and tried to re-add some routes.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,12 +13,45 @@ class MoviesController < ApplicationController
     @movie = Movie.new
   end
 
+  # I added lines 18-20, and 29-48 as per this tutorial:
+  # https://launchschool.com/books/demystifying_rails/read/create_a_record_using_a_form#creatingapost
+  #
+  # And I added lines 28-33 from another video, which I'm actually happy with.
+  #
+  # I just changed some of the words (posts > movies), which I thought was making it relevant to this project, 
+  # but I think it might be complicating stuff, and breaking it :(
+
+  def new_movie_path
+      render 'movies/new'
+    end
+
   def create
     @movie = Movie.new(params[:movie])
     if @movie.save
       redirect_to new_movie_path
     end
   end
+
+  def create_movie_path
+      insert_query = <<-SQL
+        INSERT INTO Movies (name, cinematography, scriptwriting, acting, ending, pace, soundtrack, leadactorsass, calleditness, tropes)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+      SQL
+
+      connection.execute insert_query,
+        params['title'],
+        params['cinematography'],
+        params['scriptwriting'],
+        params['acting'],
+        params['ending'],
+        params['pace'],
+        params['soundtrack'],
+        params['leadactorsass'],
+        params['calleditness'],
+        params['tropes'],
+
+      redirect_to '/index'
+    end
 
   private
     def sort_column
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
-
   resources :movies
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#homepage'
+end
+
+# These two routes are from the same tutorial I linked in the movies_controller. Also changed some
+# of the words, which might be breaking it - but I don't understand why... because I don't think I'm
+# sure what it's telling the thing to do.
+
+Rails.application.routes.draw do
+  # ...
+  get '/new_movie_path' => 'application#new_movie_path'
+end
+
+Rails.application.routes.draw do
+  # ...
+  post '/create_movie_path' => 'application#create_movie_path'
 end
